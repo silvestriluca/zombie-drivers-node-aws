bdaf4265d512a7c149881c3edb840e7e0beca68b
Luca Silvestri <luca.silvestri@amanox.ch>
2021-11-15 19:50:58 +0100

    Added ECR repositories

---
+++ iac/ecr.tf
@@ -0,0 +1,29 @@
+resource "aws_ecr_repository" "gateway_service" {
+  name                 = "zdriv-service-gateway"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags = local.global_tags
+}
+
+resource "aws_ecr_repository" "gateway_service" {
+  name                 = "zdriv-service-driver-location"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags = local.global_tags
+}
+
+resource "aws_ecr_repository" "gateway_service" {
+  name                 = "zdriv-service-zombie-detector"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+  tags = local.global_tags
+}
