333f1d2177e1669335b3e9a2973c8df381b2d069
Luca Silvestri <luca.silvestri@amanox.ch>
2021-11-15 20:09:16 +0100

    Added SNS topic and SQS queue

---
+++ iac/sns_sqs.tf
@@ -0,0 +1,22 @@
+####### SNS #######
+resource "aws_sns_topic" "drivers_updates" {
+  name              = "drivers-updates"
+  kms_master_key_id = "alias/aws/sns"
+  tags = local.global_tags
+}
+
+####### SQS #######
+resource "aws_sqs_queue" "drivers_updates_queue" {
+  name                              = "drivers-updates"
+  kms_master_key_id                 = "alias/aws/sqs"
+  kms_data_key_reuse_period_seconds = 300
+  message_retention_seconds = 86400
+  tags = local.global_tags
+}
+
+####### SQS SUBSCRIPTION TO SNS #######
+resource "aws_sns_topic_subscription" "driver_updates_sqs_target" {
+  topic_arn = aws_sns_topic.drivers_updates.arn
+  protocol  = "sqs"
+  endpoint  = aws_sqs_queue.drivers_updates_queue.arn
+}
