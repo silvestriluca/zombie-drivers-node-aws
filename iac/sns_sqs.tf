8f6ceed7403f05fe78795e5adfb5e3d7037dfe2e
Luca Silvestri <luca.silvestri@amanox.ch>
2021-11-15 20:37:18 +0100

    Disabled message enctyption for testing

--- iac/sns_sqs.tf
+++ iac/sns_sqs.tf
@@ -1,15 +1,15 @@
 ####### SNS #######
 resource "aws_sns_topic" "drivers_updates" {
   name              = "drivers-updates"
-  kms_master_key_id = "alias/aws/sns"
+  #kms_master_key_id = "alias/aws/sns"
   tags = local.global_tags
 }
 
 ####### SQS #######
 resource "aws_sqs_queue" "drivers_updates_queue" {
   name                              = "drivers-updates"
-  kms_master_key_id                 = "alias/aws/sqs"
-  kms_data_key_reuse_period_seconds = 300
+  #kms_master_key_id                 = "alias/aws/sqs"
+  #kms_data_key_reuse_period_seconds = 300
   message_retention_seconds = 86400
   tags = local.global_tags
 }
