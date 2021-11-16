161d4e7f196ab8bb7ba26c2bd68868041d601282
Luca Silvestri <luca.silvestri@amanox.ch>
2021-11-16 11:31:09 +0100

    SQS: Disabled message enctyption for testing

--- iac/sns_sqs.tf
+++ iac/sns_sqs.tf
@@ -8,8 +8,8 @@ resource "aws_sns_topic" "drivers_updates" {
 ####### SQS #######
 resource "aws_sqs_queue" "drivers_updates_queue" {
   name                              = "drivers-updates"
-  kms_master_key_id                 = "alias/aws/sqs"
-  kms_data_key_reuse_period_seconds = 300
+  #kms_master_key_id                 = "alias/aws/sqs"
+  #kms_data_key_reuse_period_seconds = 300
   message_retention_seconds = 86400
   tags = local.global_tags
 }
