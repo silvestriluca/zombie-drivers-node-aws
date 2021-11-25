a50583babfcd4061f6cfb12d5dbc4fd9a2732082
Luca Silvestri <luca.silvestri@amanox.ch>
2021-11-25 16:48:33 +0100

    Gateway: Managed process signals in server

--- services/gateway/server.js
+++ services/gateway/server.js
@@ -1,5 +1,10 @@
 #!/usr/bin/env node
 
+const process = require('process');
+// Defines Port and Host of the microservice
+const PORT = parseInt(process.env.PORT, 10) || 3000;
+const HOST = process.env.HOST || 'localhost';
+
 // creates a server
 const server = require('./microservice')({
   logger: {
@@ -9,9 +14,26 @@ const server = require('./microservice')({
 
 // Run the server!
 // eslint-disable-next-line no-unused-vars
-server.listen(3000, '0.0.0.0', (err, address) => {
+server.listen({ port: PORT, host: HOST }, (err, address) => {
   if (err) {
-    console.log(err);
+    console.error(err);
     process.exit(1);
   }
 });
+
+/**
+ * Event handler to close the server and its processes in a graceful way
+ *
+ * @param {string} signal
+ */
+async function closeGracefully(signal) {
+  console.info(`Received signal to terminate: ${signal}`);
+  await server.close();
+  // await db.close() if we have a db connection in this app
+  // await other things we should cleanup nicely
+  process.exit();
+}
+
+// Catches SIGTERM & SIGINT
+process.on('SIGTERM', closeGracefully);
+process.on('SIGINT', closeGracefully);
