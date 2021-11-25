9343967054da95c4bbe210754c381f1752e167e3
Luca Silvestri <luca.silvestri@amanox.ch>
2021-11-25 11:55:10 +0100

    Fixed more linting issues

--- services/gateway/microservice.test.js
+++ services/gateway/microservice.test.js
@@ -1,13 +1,11 @@
-'use strict'
+const build = require('./microservice');
 
-const build = require('./microservice')
-
-test('reqests the "/" route', async () =>{
-    const microservice = build()
-    const response = await microservice.inject({
-        method: 'GET',
-        url: '/'
-    })
-    expect(response.statusCode).toBe(200)
-    microservice.close()
-})
+test('reqests the "/" route', async () => {
+  const microservice = build();
+  const response = await microservice.inject({
+    method: 'GET',
+    url: '/',
+  });
+  expect(response.statusCode).toBe(200);
+  microservice.close();
+});
