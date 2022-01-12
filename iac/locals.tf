b87814e9c5588dba350dbcaf8c92c2fb46082984
Luca Silvestri <luca.silvestri@amanox.ch>
2022-01-12 18:43:56 +0100

    locals.tf: Added deploy stage map

--- iac/locals.tf
+++ iac/locals.tf
@@ -2,4 +2,9 @@ locals {
   global_tags = {
     account_id = data.aws_caller_identity.current.account_id
   }
+  deploy_stage_map = {
+    default = "dev"
+    prod    = "prod"
+  }
+  deploy_stage = local.deploy_stage_map[terraform.workspace]
 }
