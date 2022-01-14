b9ae393f6707873fd92464dd528a6645549a73c2
Luca Silvestri <luca.silvestri@amanox.ch>
2022-01-14 19:16:08 +0100

    Added ALB (W.I.P.), S3 ALB access log and SSM PS ALB switch

--- iac/ecs.tf
+++ iac/ecs.tf
@@ -111,6 +111,75 @@ resource "aws_service_discovery_private_dns_namespace" "zombie_services" {
   tags        = local.global_tags
 }
 
+################## SSM PARAMETER STORE ##################
+
+resource "aws_ssm_parameter" "alb_switch" {
+  name  = "/${var.app_name_prefix}/${terraform.workspace}/alb-switch-${local.deploy_stage}"
+  type  = "SecureString"
+  value = "false"
+  tags  = local.global_tags
+
+  lifecycle {
+    ignore_changes = [
+      value,
+    ]
+  }
+}
+
+################## ALB ##################
+#TODO
+resource "aws_lb" "drivers_alb" {
+  count              = aws_ssm_parameter.alb_switch.value == "true" ? 1 : 0
+  name               = "drivers-alb-${local.deploy_stage}"
+  internal           = false
+  load_balancer_type = "application"
+  #security_groups    = [aws_security_group.lb_sg.id]
+  subnets = [
+    aws_subnet.public_subnet_1.id,
+    aws_subnet.public_subnet_2.id,
+    aws_subnet.public_subnet_3.id,
+    aws_subnet.private_subnet_1.id,
+    aws_subnet.private_subnet_2.id,
+    aws_subnet.private_subnet_3.id
+  ]
+
+  enable_deletion_protection = false
+
+  access_logs {
+    bucket  = aws_s3_bucket.alb_access_logs_bucket.bucket
+    prefix  = "drivers-alb-${local.deploy_stage}-"
+    enabled = true
+  }
+
+  tags = local.global_tags
+}
+
+################## S3 (ALB Access logs) ##################
+
+resource "aws_s3_bucket" "alb_access_logs_bucket" {
+  bucket_prefix = "${var.app_name_prefix}-alb-logs-${local.deploy_stage}"
+  acl           = "private"
+  force_destroy = true
+  # Uses aws/s3 KMS key
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "aws:kms"
+      }
+      bucket_key_enabled = true
+    }
+  }
+  tags = local.global_tags
+}
+
+resource "aws_s3_bucket_public_access_block" "alb_access_logs_bucket" {
+  bucket                  = aws_s3_bucket.alb_access_logs_bucket.id
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
 ################## ECS ##################
 
 resource "aws_ecs_account_setting_default" "cluster_account" {
