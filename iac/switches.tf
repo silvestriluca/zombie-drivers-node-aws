d0e6c596cb5c5d19d601685586c48a06eeccb7f1
Luca Silvestri <luca.silvestri@amanox.ch>
2022-01-26 19:13:40 +0100

    switches.tf: Added SSM PS description

--- iac/switches.tf
+++ iac/switches.tf
@@ -1,10 +1,11 @@
 ################## SSM PARAMETER STORE (ONLINE ON/OFF SWITCH) ##################
 
 resource "aws_ssm_parameter" "online_switch" {
-  name  = "/${var.app_name_prefix}/${terraform.workspace}/online-switch-${local.deploy_stage}"
-  type  = "SecureString"
-  value = "false"
-  tags  = local.global_tags
+  name        = "/${var.app_name_prefix}/${terraform.workspace}/online-switch-${local.deploy_stage}"
+  description = "True to instruct Terraform to deploy ${var.app_name_verbose} online - ${local.deploy_stage} stage"
+  type        = "SecureString"
+  value       = "false"
+  tags        = local.global_tags
 
   lifecycle {
     ignore_changes = [
